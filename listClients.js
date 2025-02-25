814235e62a2c3560e5d6e70c1958d3545d0e25b5
DawK-23 <126807924+DawK-23@users.noreply.github.com>
2025-02-25 15:49:24 +0100

    Add files via upload

---
+++ listClients.js
@@ -0,0 +1,13 @@
+{  
+"Cod_Entidad": "12345", 
+"Nombre_Entidad": "Empresa X",
+"Especialidad": "IT",
+"TZ": "GMT-5",
+"Suma_2023": 1000,
+"Suma_2024": 1200,
+"Telefono": "123456789",
+"Respuesta_Entidad": "Aprobado",
+"Intentos_Totales": 3,
+"Respuesta_Agente": "Confirmado",
+"Observaciones": "Cliente satisfecho" 
+}
